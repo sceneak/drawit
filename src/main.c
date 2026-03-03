423a4d0777f0322ffbfcb4da1d82e43671210338
kueworkspace-admin <36.evan.gunardi@gonzaga.sch.id>
2026-03-04 02:08:31 +0700

    add zooming

--- src/main.c
+++ src/main.c
@@ -9,42 +9,106 @@
 #include <nanovg/nanovg.h>
 #include <nanovg/nanovg_gl.h>
 
+typedef struct { float x, y; } vec2;
+typedef struct { float x, y, z; } vec3;
+
+static float zoomFrac = 0.1f;
+
+static int screen_width, screen_height;
 static NVGcontext* vg;
+static vec2 mouse_screen;
+static vec2 mouse_world;
+static vec2 camera = {0, 0};
+static float zoom = 1.0f;
+
+static inline vec2 screen_to_world(vec2 screen)
+{
+	return (vec2){
+		.x = ( (screen.x - screen_width/2) / zoom) + camera.x,
+		.y = ( (screen_height/2 - screen.y) / zoom) + camera.y,
+	};
+}
+
+void test()
+{
+	// (World - OldCamera) * OldZoom = (World - NewCamera) * NewZoom
+}
 
 void init(void) 
 {
-    gladLoaderLoadGL();
-    vg = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES);
+	screen_width = sapp_width();
+	screen_height = sapp_height();
+	gladLoaderLoadGL();
+	vg = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES);
 }
+
 void cleanup()
 {
+	gladLoaderUnloadGL();
+	nvgDeleteGL3(vg);
+}
 
+void event(const sapp_event *e)
+{
+	switch(e->type) {
+	case SAPP_EVENTTYPE_RESIZED:
+		screen_width = sapp_width();
+		screen_height = sapp_height();
+		break;
+	case SAPP_EVENTTYPE_KEY_DOWN:
+		break;
+	case SAPP_EVENTTYPE_KEY_UP:
+		break;
+	case SAPP_EVENTTYPE_MOUSE_MOVE:
+		mouse_screen.x = e->mouse_x;
+		mouse_screen.y = e->mouse_y;
+		mouse_world = screen_to_world(mouse_screen);
+		break;
+	case SAPP_EVENTTYPE_MOUSE_DOWN:
+		break;
+	case SAPP_EVENTTYPE_MOUSE_UP:
+		break;
+	case SAPP_EVENTTYPE_MOUSE_SCROLL:
+		float ratio = (1 + zoomFrac * e->scroll_y);
+		zoom *= ratio;
+		
+		// (World - OldCamera) * OldZoom = (World - NewCamera) * NewZoom
+		camera.x = mouse_world.x - (mouse_world.x - camera.x) / ratio;
+		camera.y = mouse_world.y - (mouse_world.y - camera.y) / ratio;
+		break;
+	default:
+		break;
+	}
 }
 
 void frame(void) 
 {
-    int w = sapp_width(), h = sapp_height();
-    float dpi = sapp_dpi_scale();
+	float dpi = sapp_dpi_scale();
 
-    glViewport(0, 0, w, h);
-    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+	glViewport(0, 0, screen_width, screen_height);
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-    nvgBeginFrame(vg, w / dpi, h / dpi, dpi);
-    nvgBeginPath(vg);
-    nvgRect(vg, 100, 100, 120, 30);
-    nvgFillColor(vg, nvgRGBA(255, 192, 0, 255));
-    nvgFill(vg);
-    nvgEndFrame(vg);
+	nvgBeginFrame(vg, screen_width/dpi, screen_height/dpi, dpi);
+	nvgTranslate(vg, screen_width/2, screen_height/2);
+	nvgScale(vg, zoom, zoom);
+	nvgTranslate(vg, -camera.x, camera.y);
+
+		nvgBeginPath(vg);
+			nvgRect(vg, -25, -25, 50, 50);
+		nvgFillColor(vg, nvgRGBA(255, 192, 0, 255));
+		nvgFill(vg);
+
+	nvgEndFrame(vg);
 }
 
 sapp_desc sokol_main(int argc, char* argv[]) {
-    return (sapp_desc){
-        .init_cb = init,
-        .frame_cb = frame,
-        .cleanup_cb = NULL,
-        .width = 800,
-        .height = 600,
-        .window_title = "My Sokol Window",
-    };
+	return (sapp_desc){
+		.init_cb      = init,
+		.frame_cb     = frame,
+		.event_cb     = event,
+		.cleanup_cb   = cleanup,
+		.window_title = "Drawit",
+		
+	};
 }
