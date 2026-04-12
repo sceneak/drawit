a2b2d1e45a9937d7bf289d793224478636d54a35
SceneAK <no@email.com>
2026-04-12 08:49:34 +0700

    add append_many and fix indentations

--- src/ds.h
+++ src/ds.h
@@ -12,36 +12,53 @@
 #define DA_DEFINE(type, name)                                                                                   \
 	struct name                                                                                             \
 	{                                                                                                       \
-		size_t capacity;                                                                                \
-		size_t len;                                                                                     \
-		type elems[];                                                                                   \
+	        size_t capacity;                                                                                \
+	        size_t len;                                                                                     \
+	        type elems[];                                                                                   \
 	};                                                                                                      \
 	NO_DISCARD static inline struct name *name##_create(size_t capacity)                                    \
 	{                                                                                                       \
-		assert(capacity != 0);                                                                          \
-		struct name *alloc = (struct name *)calloc(1, sizeof(struct name) + capacity * sizeof(type));   \
-		if (!alloc)                                                                                     \
-			fputs("Allocation failure: buy more ram lol", stderr);                                  \
-		else                                                                                            \
-			alloc->capacity = capacity;                                                             \
-		return alloc;                                                                                   \
+	        assert(capacity != 0);                                                                          \
+	        struct name *alloc = (struct name *)calloc(1, sizeof(struct name) + capacity * sizeof(type));   \
+	        if (!alloc)                                                                                     \
+	                fputs("Allocation failure: buy more ram lol", stderr);                                  \
+	        else                                                                                            \
+	                alloc->capacity = capacity;                                                             \
+	        return alloc;                                                                                   \
+	}
+	NO_DISCARD static inline struct name *name##_expand(struct name *da, int extra)                 \
+	{                                                                                               \
+	        struct name *temp;                                                                      \
+	        int new_capacity = da->capacity;                                                        \
+	                                                                                                \
+	        while (da->len + extra >= new_capacity) new_capacity *= 2;                              \
+	        if (new_capacity == da->capacity)                                                       \
+	                return da;                                                                      \
+	                                                                                                \
+	        struct name *temp = realloc(da, sizeof(struct name) + new_capacity * sizeof(type);      \
+	        if (!temp)                                                                              \
+	        {                                                                                       \
+	                fputs("Allocation failure: buy more ram lol", stderr);                          \
+	                return da;                                                                      \
+	        }                                                                                       \
+	        da = temp;                                                                              \
+	        da->capacity = new_capacity;                                                            \
+	        return da;                                                                                      \
 	}                                                                                                       \
 	NO_DISCARD static inline struct name *name##_append(struct name *da, type elem)                         \
 	{                                                                                                       \
-		if (da->len >= da->capacity)                                                                    \
-		{                                                                                               \
-			struct name *temp = realloc(da, sizeof(struct name) + da->capacity * 2 * sizeof(type)); \
-			if (!temp)                                                                              \
-			{                                                                                       \
-				fputs("Allocation failure: buy more ram lol", stderr);                          \
-				return da;                                                                      \
-			}                                                                                       \
-			da = temp;                                                                              \
-			da->capacity *= 2;                                                                      \
-		}                                                                                               \
-		da->elems[da->len++] = elem;                                                                    \
-		return da;                                                                                      \
-	}                                                                                                       \
-
+	        name##_expand(da, 1);                                                                           \
+	        da->elems[da->len-1] = elem;                                                                    \
+	        return da;                                                                                      \
+	}
+	NO_DISCARD static inline struct name *name##_append_many(struct name *da, type elem[], int len)         \
+	{                                                                                                       \
+	        int i;                                                                                          \
+	        name##_expand(da, len);                                                                         \
+	        for (i = 0; i < len; i++)                                                                       \
+	                da->elems[i] = elem[i];                                                                 \
+	        return da;                                                                                      \
+	}
+                                                                                                                
 #endif
 
